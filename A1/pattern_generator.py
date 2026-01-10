ba0dc6d05dbc4e68043000f51dbfb4217484efd2
niryt20 <niryt20@student.sdu.dk>
2026-01-10 17:01:29 +0100

    Setting up canvas and initial input parameters

--- A1/pattern_generator.py
+++ A1/pattern_generator.py
@@ -1,3 +1,19 @@
-# import numpy libraries
+## Importing numpy libraries
 import numpy as np
 import matplotlib.pyplot as plt
+
+## Sin function frequency
+ff = (1/(1.5*3.14))
+
+## Number of attractor points
+attr_n = 7
+
+## Canvas dimensions
+width = 200
+height = 200
+
+## Setting up the canvas
+canvas = np.zeros((height, width)) 
+
+## creating a given number of random attractor points
+attractors = np.random.random((attr_n, 2)) * 200
