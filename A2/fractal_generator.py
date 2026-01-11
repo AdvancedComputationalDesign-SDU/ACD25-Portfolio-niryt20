534b0c077dfd89749d07e308e0ddf9cfa46e6fae
niryt20 <niryt20@student.sdu.dk>
2026-01-11 12:12:25 +0100

    Adding biased growth towards attractor point in growth function

--- A2/fractal_generator.py
+++ A2/fractal_generator.py
@@ -35,7 +35,7 @@ V = (0, 1)
 Lines = []
 ## Function for rotating a given vector
 def rotate_vector(v, angle_deg):
-    angle_red = math.radians(angle_deg)
+    angle_rad = math.radians(angle_deg)
     x, y = v
     xr = x * math.cos(angle_rad) - y * math.sin(angle_rad)
     yr = x * math.sin(angle_rad) + y * math.cos(angle_rad)
@@ -55,22 +55,38 @@ def length_field(pt):
     d = math.hypot(pt[0] - attractor.x, pt[1] - attractor.y)
     return max(min_length, max_length - length_factor * d)
 
+## Growth function
 def Grow(pt, v, g):
+    if g < gen:
 
-    V1 = rotate_vector(v, -ini_angle)
-    pt1 = (pt[0] + V1[0]*L, pt[1] + V1[1]*L)
+        # Vector toward attractor
+        to_attr = (attractor.x - pt[0], attractor.y - pt[1])
 
-    V2 = rotate_vector(v, ini_angle)
-    pt2 = (pt[0] + V2[0]*L, pt[1] + V2[1]*L)
+        # Biased growth towards attractor point
+        v = (v[0] + attractor_strength * to_attr[0], v[1] + attractor_strength * to_attr[1],)
 
-    L1 = LineString([pt, pt1])
-    L2 = LineString([pt, pt2])
+        v = normalize(v)
 
-    Lines.append(L1)
-    Lines.append(L2)
+        V1 = rotate_vector(v, -ini_angle)
+        pt1 = (pt[0] + V1[0]*L, pt[1] + V1[1]*L)
 
-    Grow(pt1, V1, g + 1)
-    Grow(pt2, V2, g + 1)
+        V2 = rotate_vector(v, ini_angle)
+        pt2 = (pt[0] + V2[0]*L, pt[1] + V2[1]*L)
+
+        L1 = LineString([pt, pt1])
+        L2 = LineString([pt, pt2])
+
+        Lines.append(L1)
+        Lines.append(L2)
+
+        Grow(pt1, V1, g + 1)
+        Grow(pt2, V2, g + 1)
+
+## 1st branch Generation
+B = (A[0] + V[0], A[1] + V[1])
+Lines.append(LineString([A, B]))
+
+Grow(B, V, 0)
 
 ## Visualization
 fig, ax = plt.subplots()
