6aabe713a1070a17378e746c8df68b808f53f544
niryt20 <niryt20@student.sdu.dk>
2026-01-11 22:02:31 +0100

    defined initial UV grid function

--- A4/surface_generator.py
+++ A4/surface_generator.py
@@ -23,42 +23,11 @@ import random
 O = (orix,oriy,oriz)
 S = (sizex,sizey)
 
-# -----------------------------------------------------------------------------
-# 1. Heightmap Generation
-# -----------------------------------------------------------------------------
-def generate_heightmap(shape=(50, 50), params=None):
-    """Create a heightmap array from input data."""
-    # TODO: generate and return a NumPy heightmap array
-    raise NotImplementedError("Implement generate_heightmap(...)")
+### initial UV grid
+def uv_grid(divU, divV):
+    u = np.linspace(0.0, 1.0, divU)
+    v = np.linspace(0.0, 1.0, divV)
+    U, V = np.meshgrid(u, v)
 
+    return U, V
 
-# -----------------------------------------------------------------------------
-# 2. Uniform Surface Sampling
-# -----------------------------------------------------------------------------
-def sample_surface_uniform(surface, shape=(50, 50)):
-    """Sample a surface uniformly in its UV domain."""
-    # TODO: generate UV grid, evaluate surface points, return as NumPy array
-    raise NotImplementedError("Implement sample_surface_uniform(...)")
-
-
-# -----------------------------------------------------------------------------
-# 3. Grid Manipulation (apply heightmap to grid)
-# -----------------------------------------------------------------------------
-def manipulate_point_grid(heightmap, point_grid, scalar):
-    """Apply heightmap values to a sampled point grid."""
-    # TODO: modify point_grid using heightmap and return it
-    raise NotImplementedError("Implement manipulate_point_grid(...)")
-
-
-# -----------------------------------------------------------------------------
-# 4. Build Surface from Manipulated Points
-# -----------------------------------------------------------------------------
-def build_surface(point_grid):
-    """Build a surface from a point grid."""
-    # TODO: use Rhino.Geometry to construct a surface from the point grid
-    raise NotImplementedError("Implement build_surface(...)")
-
-heightmap = generate_heightmap(shape=(U,V))
-pt_grid = sample_surface_uniform(surface, shape=(U,V))
-manip_pt_grid = manipulate_point_grid(heightmap, pt_grid, scalar)
-manip_srf = build_surface(pt_grid)
